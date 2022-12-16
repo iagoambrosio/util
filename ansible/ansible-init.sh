ddfb75bd619abb1446c9bd66087dcf20979b6129
Iago silva ambrosio <iago_ambrosio@outlook.com>
2022-12-16 13:29:38 -0300

    Automatização da identificação do gerenciador de pacote do sistema

--- ansible/ansible-init.sh
+++ ansible/ansible-init.sh
@@ -1,15 +1,24 @@
-#!/bin/bash
-declare -A osInfo;
-osInfo[/etc/debian_version]="apt"
-osInfo[/etc/alpine-release]="apk"
-osInfo[/etc/centos-release]="yum"
-osInfo[/etc/fedora-release]="dnf"
-
-for f in ${!osInfo[@]}
-do
-    if [[ -f $f ]];then
-        package_manager=${osInfo[$f]}
-    fi
-done
-
-sudo $package_manager install python3
+#!/bin/sh
+  PACKAGE=python3
+  YUM_CMD=$(whereis yum)
+  APT_GET_CMD=$(which apt-get)
+  PACMAN_CMD=$(which pacman)
+  EMERGE_CMD=$(which emerge)
+  ZYPP_CMD=$(which zypper)
+  APK_CMD=$(which apk)
+ if [[ ! -z $YUM_CMD ]]; then
+    yum install -y $PACKAGE
+ elif [[ ! -z $APT_GET_CMD ]]; then
+    apt-get install $PACKAGE
+ elif [[ ! -z $PACMAN_CMD ]]; then
+    pacman -Sy $PACKAGE
+ elif [[ ! -z $EMERGE_CMD ]]; then 
+    emerge --ask dev-lang/python:3.8
+ elif [[ ! -z $APK_CMD ]]; then
+    apk add $PACKAGE
+ elif [[ ! -z $ZYPP_CMD ]]; then
+    zypper install -y $PACKAGE
+ else
+    echo "error can't install package $PACKAGE"
+    exit 1;
+ fi
