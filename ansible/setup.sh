3387aeea633c9a76c7fb1c411af01b7df8532037
iagoambrosio <iago_ambrosio@outlook.com>
2022-12-27 19:53:16 -0300

    Melhoramento do setup e desligamento do swapp das máquinas

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -1,3 +1,9 @@
-apt install ansible sshpass
+#!/bin/bash
+#Rodar esse script para preparar o ambiente
+#
+sudo apt install ansible sshpass
 ssh-keygen -f id_rsa -P ""
 cp id_rsa $HOME/.ssh/ 
+cp ansible.cfg /etc/ansible/
+
+#Após a execução do playbook 2, trocar o usuário para "ansible" para manter os playbooks funcionais
